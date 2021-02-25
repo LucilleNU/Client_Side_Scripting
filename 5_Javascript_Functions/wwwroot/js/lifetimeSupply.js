4d25190e648336de89d4994656e535e19592b0ae
Nawa Lucille <nawalucille@gmail.com>
2021-02-26 01:28:28 +0200

    Tasks

---
+++ 5_Javascript_Functions/wwwroot/js/lifetimeSupply.js
@@ -0,0 +1,12 @@
+﻿function calcSupply(age, maxAge, food, foodPerDay) {
+    var totalNeeded = (foodPerDay * 365) * (maxAge - age);
+    totalNeeded = totalNeeded.toFixed(0);
+    var message = totalNeeded + 'kg of ' +food + ' would be enough until I am ' +maxAge + ' years old.';
+    console.log(message);
+}
+
+calcSupply(38, 118, 'chocolate', 0.5);
+calcSupply(20, 87, 'fruits', 2);
+calcSupply(16, 102, 'nuts', 1.1);
+
+
