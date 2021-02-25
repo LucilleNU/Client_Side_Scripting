b48da7d25ef0286c20d0576c0a78d1b83a9bdd83
Nawa Lucille <nawalucille@gmail.com>
2021-02-26 01:03:46 +0200

    Tasks

---
+++ 5_Javascript_Functions/wwwroot/js/calculateknot.js
@@ -0,0 +1,10 @@
+﻿function convertToKnot() {
+    let a = Number(document.getElementById("num1").value);
+
+    //1 km/h to knots = 0.53996 knots
+    var answer = a * 0.53996;
+    answer = answer.toFixed(2);
+
+    document.getElementById("results").innerHTML = "Number in knots: " + answer;
+
+}
