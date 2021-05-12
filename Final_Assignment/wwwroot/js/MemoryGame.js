897103189770e98a4615dc42768a9476052fa4c8
Nawa Lucille <nawalucille@gmail.com>
2021-05-12 11:27:24 +0300

    Update

--- Final_Assignment/wwwroot/js/MemoryGame.js
+++ Final_Assignment/wwwroot/js/MemoryGame.js
@@ -1,27 +1,20 @@
-﻿var theme;
-var size;
-var players;
-var BoxOpened = "";
-var ImgOpened = "";
-var Counter = 0;
-var ImgFound = 0;
-let Score = document.getElementsByClassName("score"); /*players' points*/
-const board = document.getElementById("boxcard");// deck of all cards in game
-let startmenu = document.getElementById("popup0");
-let cards = document.querySelectorAll(' .board .card');
+﻿let moves = 0;
+let matchedCard = document.getElementsByClassName("match");
+let closeicon = document.querySelector(".close");
 let closemenu = document.getElementById("closemenu");
-let closeicon = document.querySelector(".close");// close icon in modal
-let playerScore1 = document.getElementById("score1");  //1 player points 
+let modal = document.getElementById("popup1")// declare modal
+const board = document.getElementById("boxcard");
+let cards = document.querySelectorAll(' .board .card');
+let congrats = document.getElementById("popup1")
+let hasFlipedCard = false;
+let firstCard, secondCard;
+let images = document.getElementsByClassName("front");
+let startmenu = document.getElementById("popup0");
+let Score = document.getElementsByClassName("score"); 
+let playerScore1 = document.getElementById("score1"); 
+var turn = 1; 
+let names = document.getElementsByClassName("name"); 
 
-var Source = "#boxcard";
-
-var ImgSource = [];
-
-var turn = 1; //index of curent player
-
-let names = document.getElementsByClassName("name"); //player names
-
-//player forms
 
 let formPlayers = [
 	document.getElementById("1player"),
@@ -29,7 +22,7 @@ let formPlayers = [
 	document.getElementById("3player"),
 	document.getElementById("4player")];
 
-let nameScores = document.getElementsByClassName("name-score"); //display player form
+let nameScores = document.getElementsByClassName("name-score"); 
 
 
 function GetPlayersName() {
@@ -43,7 +36,6 @@ function GetPlayersName() {
 	names[3].innerHTML = player4;
 }
 
-//player-points storage
 var PlayersPoints = [
 	{ Player: names[0], Points: Score[0].value },
 	{ Player: names[1], Points: Score[1].value },
@@ -57,7 +49,6 @@ let currentPlayer = PlayersPoints[0].Player;
 document.getElementById("content2").style.display = "inline";
 let radiobtns = document.getElementsByClassName("radiobutton");
 let chosen = 1; //amount of players
-
 radiobtns[0].checked = true;
 function Multiplayer() {
 	for (var i = 0; i < radiobtns.length; i++) {
@@ -66,7 +57,6 @@ function Multiplayer() {
 	this.checked = true;
 	chosen = Number(this.value);
 	DisplayPlayers(); //display forms
-
 }
 
 function DisplayPlayers() {
@@ -81,38 +71,13 @@ function DisplayPlayers() {
 		nameScores[j].style.display = "block";
 	}
 }
-//display form when button clicked
+
 for (var i = 0; i < radiobtns.length; i++) {
 	if (radiobtns[i].disabled === false) {
 		radiobtns[i].addEventListener("click", Multiplayer);
 	}
 }
 
-document.body.onload = StartMenu(0);
-function StartMenu(index) {
-	$(board).css({ "visibility": "hidden" }); //hide playground
-	$(".players").css({ "visibility": "hidden" }); //hide players' forms
-	startmenu.classList.add("show"); //display start menu
-
-	//remove all markers to restart game
-	if (index == 0) {
-		for (var i = 0; i < cards.length; i++) {
-			cards[i].classList.remove("show", "open", "match", "disabled");
-		}
-	}
-	//hide all cards NOT to restart game
-	else {
-		for (var i = 0; i < cards.length; i++) {
-			cards[i].style.display = "none";
-		}
-		for (var i = 0; i < radiobtns.length; i++) {
-			radiobtns[i].disabled = true;
-		}
-		CloseMenu();
-	}
-}
-
-//closes menu, but DOSEN'T restart the game, ignoring new parameters (except names)
 function CloseMenu() {
 	document.getElementById("closemenu").style.display = "block";
 	closemenu.addEventListener("click", function (e) {
@@ -128,73 +93,88 @@ function CloseMenu() {
 }
 
 
+document.body.onload = StartMenu(0);
+function StartMenu(index) {
+	$(board).css({ "visibility": "hidden" }); 
+
+	$(".players").css({ "visibility": "hidden" });
+
+	startmenu.classList.add("show");
+
+	if (index == 0) {
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].classList.remove("show", "open", "match", "disabled");
+		}
+	}
+	
+	else {
+		for (var i = 0; i < cards.length; i++) {
+			cards[i].style.display = "none";
+		}
+		for (var i = 0; i < radiobtns.length; i++) {
+			radiobtns[i].disabled = true;
+		}
+		CloseMenu();
+	}
+}
+
+var selected = "cats";
+
+let options = document.getElementsByClassName("option");
+
+function ChosenOption() {
+	this.classList.add("selected");
+	selected = this.id;
+	console.log(selected);
+	for (var i = 0; i < options.length; i++) {
+		option = options[i];
+		if (option.id !== selected) {
+			option.classList.remove("selected");
+		}
+	}
+}
+for (var i = 0; i < options.length; i++) {
+	options[i].addEventListener("click", ChosenOption);
+}
+
+//change playground images theme, based on user's choice
 function Theme() {
-	theme = prompt("Enter a card theme", "Cats, Dogs or Dinosaurs");
-	if (theme != null) {
-		document.getElementById("themeselect").innerHTML = theme;
+	var Theme = [];
 
+	var ThemeAlts = [];
+	for (var i = 0; i < 10; i++) {
+		Theme[i] = new Image();
+		Theme[i].src = "Images/" + selected + "/img" + (i + 1) + ".png";
+		Theme[i].alt = "img" + (i + 1);
+		ThemeAlts[i] = Theme[i].alt;
 	}
-	if (theme == "Cats") {
-		ImgSource = [
-			"Images/cats/cat1.svg",
-			"Images/cats/cat2.svg",
-			"Images/cats/cat3.svg",
-			"Images/cats/cat4.svg",
-			"Images/cats/cat5.svg",
-			"Images/cats/cat6.svg",
-			"Images/cats/cat7.svg",
-			"Images/cats/cat8.svg",
-			"Images/cats/cat9.svg",
-			"Images/cats/cat10.svg",
-		];
+	var imgAlts = [];
+	for (var j = 0; j < 20; j++) {
+		imgAlts[j] = images[j].alt;
+		if (ThemeAlts.includes(imgAlts[j])) {
+			let index = ThemeAlts.indexOf(imgAlts[j]);
+			images[j].src = Theme[index].src;
+		}
 	}
-	if (theme == "Dogs") {
-		ImgSource = [
-			"Images/dogs/dog1.png",
-			"Images/dogs/dog2.jpeg",
-			"Images/dogs/dog3.jpeg",
-			"Images/dogs/dog4.png",
-			"Images/dogs/dog5.jpeg",
-			"Images/dogs/dog6.jpeg",
-			"Images/dogs/dog7.jpeg",
-			"Images/dogs/dog8.png",
-			"Images/dogs/dog9.jpeg",
-			"Images/dogs/dog10.jpeg",
-		];
-	}
-	if (theme == "Dinosaurs") {
-		ImgSource = [
-			"Images/dinosaurs/dinosaur1.jpeg",
-			"Images/dinosaurs/dinosaur2.png",
-			"Images/dinosaurs/dinosaur3.png",
-			"Images/dinosaurs/dinosaur4.png",
-			"Images/dinosaurs/dinosaur5.png",
-			"Images/dinosaurs/dinosaur6.png",
-			"Images/dinosaurs/dinosaur7.jpeg",
-			"Images/dinosaurs/dinosaur8.jpeg",
-			"Images/dinosaurs/dinosaur9.jpeg",
-			"Images/dinosaurs/dinosaur10.jpeg",
-		];
-    }
 }
 
-function size()
+//shuffle cards
+function shuffle(cards) {
+	cards.forEach(card => {
+		let randomPos = Math.floor(Math.random() * 20);
+		card.style.order = randomPos;
+	});
+};
+
+function StartGame()
 {
-	size = prompt("Enter field size. The bigger the board - the more cards will there be to remember", "3x4, 4x4 or 5x4");
-	if (size != null) {
-		document.getElementById("size").innerHTML = size;
-
-	}
-}
-
-function StartGame() {
+	hasFlipedCard = false;
 	document.getElementById("namescore1").style.display = "block";
 	GetPlayersName();
 	startmenu.classList.remove("show");
 
 
-	playerScore1.value = 0; //refresh points counter for 1 player mod 
-	//refresh points counter for multiplayer mode 
+	playerScore1.value = 0; 
 	for (var i = 0; i < PlayersPoints.length; i++) {
 		PlayersPoints[i].Points = 0;
 		Score[i].value = 0;
@@ -213,106 +193,156 @@ function StartGame() {
 
 	firstCard = 0;
 	secondCard = 0;
+	Theme();
+	shuffle(cards);
 
-	ShuffleImages();
+	// remove all exisiting classes from each card
+	for (var i = 0; i < cards.length; i++) {
+		board.innerHTML = "";
+		[].forEach.call(cards, function (item) {
+			board.appendChild(item);
+		});
+		cards[i].style.display = "block";
+		cards[i].classList.remove("show", "open", "match", "disabled");
+
+		moves = 0;
+	}
 }
 
-function RandomFunction(MaxValue, MinValue) {
-	return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+//check if card open and make it first or second card
+
+function flipCard() {
+	this.classList.toggle("open");
+	this.classList.toggle("show");
+	this.classList.toggle("disabled");
+
+	if (!hasFlipedCard) {
+		hasFlipedCard = true;
+		firstCard = this;
+	}
+	else {
+		disablePlayGround();
+		hasFlipedCard = false;
+		secondCard = this;
+		checkForMatch(firstCard);
+		moveCounter();
+	}
 }
-	function ShuffleImages() {
-		var ImgAll = $(Source).children();
-		var ImgThis = $(Source + " div:first-child");
-		var ImgArr = new Array();
 
-		for (var i = 0; i < ImgAll.length; i++) {
-			ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
-			ImgThis = ImgThis.next();
-		}
-
-		ImgThis = $(Source + " div:first-child");
-
-		for (var z = 0; z < ImgAll.length; z++) {
-			var RandomNumber = RandomFunction(0, ImgArr.length - 1);
-
-			$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
-			ImgArr.splice(RandomNumber, 1);
-			ImgThis = ImgThis.next();
-		}
+function checkForMatch() {
+	if (firstCard.dataset.framework === secondCard.dataset.framework) {
+		matched();
 	}
-
-	function ResetGame() {
-		ShuffleImages();
-		$(Source + " div img").hide();
-		$(Source + " div").css("visibility", "visible");
-		Counter = 0;
-		$("#success").remove();
-		$("#counter").html("" + Counter);
-		BoxOpened = "";
-		ImgOpened = "";
-		ImgFound = 0;
-		return false;
+	else {
+		unmatched();
 	}
+}
 
-	function OpenCard() {
-		var id = $(this).attr("id");
+function matched() {
+	firstCard.classList.add("match", "disabled");
+	secondCard.classList.add("match", "disabled");
 
-		if ($("#" + id + " img").is(":hidden")) {
-			$(Source + " div").unbind("click", OpenCard);
+	if (chosen === 1) {
+		playerScore1.value++;
+	}
+	else {
 
-			$("#" + id + " img").slideDown('fast');
+		for (var i = 0; i < PlayersPoints.length; i++) {
 
-			if (ImgOpened == "") {
-				BoxOpened = id;
-				ImgOpened = $("#" + id + " img").attr("src");
-				setTimeout(function () {
-					$(Source + " div").bind("click", OpenCard)
-				}, 300);
-			}
-
-			else {
-				CurrentOpened = $("#" + id + " img").attr("src");
-				if (ImgOpened != CurrentOpened) {
-					setTimeout(function () {
-						$("#" + id + " img").slideUp('fast');
-						$("#" + BoxOpened + " img").slideUp('fast');
-						BoxOpened = "";
-						ImgOpened = "";
-					}, 400);
-				}
-
-				else {
-					$("#" + id + " img").parent().css("visibility", "hidden");
-					$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
-					ImgFound++;
-					BoxOpened = "";
-					ImgOpened = "";
-				}
-				setTimeout(function () {
-					$(Source + " div").bind("click", OpenCard)
-				}, 400);
-			}
-			Counter++;
-			$("#counter").html("" + Counter);
-
-			if (ImgFound == ImgSource.length) {
-				$("#counter").prepend('<span id="success">You Found All Pictues With </span>');
+			if (currentPlayer.innerHTML == PlayersPoints[i].Player.innerHTML) {
+				PlayersPoints[i].Points++;
+				Score[i].value++;
 			}
 		}
 	}
+	AblePlayGround();
+}
 
-	$(document).ready(function () {
+function unmatched() {
 
-		for (var y = 1; y < 3; y++) {
-			$.each(ImgSource, function (i, val) {
-				$(Source).append("<div id=card" + y + i + "><img src=" + val + " />");
-			});
-		}
-		$(Source + " div").click(OpenCard);
-		ShuffleImages();
+	firstCard.classList.add("unmatched");
+	secondCard.classList.add("unmatched");
+	setTimeout(function () {
+		firstCard.classList.remove("open", "unmatched");
+		secondCard.classList.remove("open", "unmatched");
+		enable();
+	}, 850);
+	setTimeout(function () {
+		firstCard.classList.remove("show");
+		secondCard.classList.remove("show");
+		AblePlayGround();
+	}, 900);
+
+}
+
+//disable all cards
+function disablePlayGround() {
+	for (var c = 0; c < cards.length; c++) {
+		card = cards[c];
+		card.classList.add("disableplayground");
 	}
-	);
+}
+//unblock plauground
+function AblePlayGround() {
+	for (var c = 0; c < cards.length; c++) {
+		card = cards[c];
 
+		card.classList.remove("disableplayground");
+	}
+}
+
+
+//enable cards and disable matched cards
+function enable() {
+	Array.prototype.filter.call(cards, function (card) {
+		card.classList.remove('disabled');
+		for (var i = 0; i < matchedCard.length; i++) {
+			matchedCard[i].classList.add("disabled");
+		}
+	});
+}
+
+//count player's moves
+function moveCounter() {
+	moves++;
+	//after one move ==> next player's turn (until all cards will be opened)
+	if (matchedCard.length !== 20) {
+		if (chosen == 4 && turn == 4) {
+			PlayersPoints[3].Player.classList.remove("turn");
+			turn = 5;
+		}
+
+		if (turn <= chosen) {
+			PlayersPoints[turn - 1].Player.classList.remove("turn");
+			PlayersPoints[turn].Player.classList.add("turn");
+			currentPlayer = PlayersPoints[turn].Player;
+			turn++;
+		}
+		if (turn > chosen) {
+			PlayersPoints[chosen - 1].Player.classList.remove("turn");
+			PlayersPoints[0].Player.classList.add("turn")
+			currentPlayer = PlayersPoints[0].Player;
+			turn = 1;
+		}
+	}
+}
+
+//congratulations when all cards match, show modal and moves, time and rating
+function congratulations() {
+	if (matchedCard.length == 20) {
+		var b = 0;
+		while (b < 4) {
+			PlayersPoints[b].Player.classList.remove("turn");
+			b++
+		}
+
+		clearInterval(interval);
+		finalTime = timer.innerHTML;
+
+		congrats.classList.add("show"); // show congratulations popup
+
+	}
+}
 
 function close() {
 	closeicon.addEventListener("click", function (e) {
